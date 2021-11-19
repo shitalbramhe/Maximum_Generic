32289fd71683ceb7582bd0ef3de5cfe01bfdee62
shital bramhe <shitalbramhe@gmail.com>
2021-11-19 14:28:39 +0530

    [shital] ADD: find maximum number from three integer values

---
+++ MaximumGeneric/MaximumGeneric/Findmaximumnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumGeneric
+{
+    public class Findmaximumnum
+    {
+        public int Computemax(int firstnumber, int secondnumber, int thirdnumber)
+        {
+            if (firstnumber.CompareTo(secondnumber) > 0 && firstnumber.CompareTo(thirdnumber) > 0)
+            {
+                return firstnumber;
+            }
+            if (secondnumber.CompareTo(firstnumber) > 0 && secondnumber.CompareTo(thirdnumber) > 0)
+            {
+                return secondnumber;
+            }
+            else
+            {
+                return thirdnumber;
+            }
+        }
+    }
+}
--- MaximumGeneric/MaximumGeneric/Program.cs
+++ MaximumGeneric/MaximumGeneric/Program.cs
@@ -10,8 +10,27 @@ namespace MaximumGeneric
     {
         static void Main(string[] args)
         {
+            Findmaximumnum Max = new Findmaximumnum();
+            bool flag = true;
+            while (flag)
             {
                 Console.WriteLine("Welcome to Generic maximum problems");
+                Console.WriteLine("Enter choice of program : \n1. Maximum integer \n2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first number, second number,third number");
+                        int firstnumber = Convert.ToInt32(Console.ReadLine());
+                        int secondnumber = Convert.ToInt32(Console.ReadLine());
+                        int thirdnumber = Convert.ToInt32(Console.ReadLine());
+                        int result = Max.Computemax(firstnumber, secondnumber, thirdnumber);
+                        Console.WriteLine("The maximum number is : " + result);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
             }
         }
     }
