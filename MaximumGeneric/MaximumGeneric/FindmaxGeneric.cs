eb2a2094ac6ce5959ff9248d9f60daa56f518a1e
shital bramhe <shitalbramhe@gmail.com>
2021-11-19 19:53:18 +0530

    [shital] ADD: find maximum using generic

---
+++ MaximumGeneric/MaximumGeneric/FindmaxGeneric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumGeneric
+{
+    public class FindmaxGeneric<T>
+    {
+        public static string Computemaxgeneric<T>(T firstvalue, T secondvalue, T thirdvalue) where T : IComparable<T>
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+            {
+                return firstvalue.ToString(); 
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+            {
+                return secondvalue.ToString();
+            }
+            else
+            {
+                return thirdvalue.ToString();
+            }
+        }
+    }
+}
--- MaximumGeneric/MaximumGeneric/Program.cs
+++ MaximumGeneric/MaximumGeneric/Program.cs
@@ -15,7 +15,7 @@ namespace MaximumGeneric
             while (flag)
             {
                 Console.WriteLine("Welcome to Generic maximum problems");
-                Console.WriteLine("Enter choice of program : \n1. Maximum integer \n2. Maximum Float \n3. Exit");
+                Console.WriteLine("Enter choice of program : \n1. Maximum integer \n2. Maximum Float \n3. Maximum String \n4. Refactor using Generic \n5. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -36,6 +36,22 @@ namespace MaximumGeneric
                         Console.WriteLine("The maximum number is : " + result2);
                         break;
                     case 3:
+                        Console.WriteLine("Enter first word, second word,third word");
+                        string first = Convert.ToString(Console.ReadLine());
+                        string second = Convert.ToString(Console.ReadLine());
+                        string third = Convert.ToString(Console.ReadLine());
+                        string result3 = Max.Computemaximum(first, second, third);
+                        Console.WriteLine("The maximum number is : " + result3);
+                        break;
+                    case 4:
+                        string result4 = FindmaxGeneric<int>.Computemaxgeneric<int>(10,20,30);
+                        Console.WriteLine("The maximum number is : " + result4);
+                        string result5 = FindmaxGeneric<float>.Computemaxgeneric<float>(10.8f, 20.6f, 30.9f);
+                        Console.WriteLine("The maximum number is : " + result5);
+                        string result6 = FindmaxGeneric<string>.Computemaxgeneric<string>("sanket","shital","prajakta");
+                        Console.WriteLine("The maximum number is : " + result6);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
